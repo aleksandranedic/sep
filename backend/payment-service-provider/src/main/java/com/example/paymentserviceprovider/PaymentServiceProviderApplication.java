c4e810bc4a563edbc527c28e3c7ce6cba7e1c173
Anđela <andjelamiskovic2000@gmail.com>
2023-11-12 15:43:45 +0100

    services

---
+++ backend/payment-service-provider/src/main/java/com/example/paymentserviceprovider/PaymentServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceProviderApplication.class, args);
+    }
+
+}
