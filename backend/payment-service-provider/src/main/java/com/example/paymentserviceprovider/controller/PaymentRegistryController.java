59c07c417a91c0af95db10c93e469ddbecaa1473
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2023-11-21 10:00:59 +0100

    add payment registry database

---
+++ backend/payment-service-provider/src/main/java/com/example/paymentserviceprovider/controller/PaymentRegistryController.java
@@ -0,0 +1,19 @@
+package com.example.paymentserviceprovider.controller;
+
+import com.example.paymentserviceprovider.service.PaymentRegistryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "registry/payment")
+public class PaymentRegistryController {
+
+    @Autowired
+    PaymentRegistryService paymentRegistryService;
+    @GetMapping("/refresh")
+    public void refreshPaymentServices() {
+        paymentRegistryService.loadPaymentRegistry();
+    }
+}
