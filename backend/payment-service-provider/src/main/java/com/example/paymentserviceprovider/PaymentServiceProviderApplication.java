6251a967d861f00c9a66e9835e45b6aa801df02e
Anđela <andjelamiskovic2000@gmail.com>
2023-12-24 20:36:04 +0100

    eureka

--- backend/payment-service-provider/src/main/java/com/example/paymentserviceprovider/PaymentServiceProviderApplication.java
+++ backend/payment-service-provider/src/main/java/com/example/paymentserviceprovider/PaymentServiceProviderApplication.java
@@ -5,8 +5,10 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentServiceProviderApplication {
 
     @Autowired
