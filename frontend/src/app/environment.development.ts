859367d4a3b83f87e0bacd5302434a5fb3e2b3bf
Anđela <andjelamiskovic2000@gmail.com>
2024-01-31 23:21:43 +0100

    update PCI DSS

--- frontend/src/app/environment.development.ts
+++ frontend/src/app/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8086',
-  authUrl: 'https://localhost:8001',
+  authUrl: 'http://localhost:8001',
   bankUrl: 'http://localhost:8000'
 };
